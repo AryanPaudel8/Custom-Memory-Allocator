4a5bf4ff5f48ce4cc91e0b40a89a65cb074f47d2
AryanPaudel8 <rnrn97967@gmail.com>
2026-03-10 19:06:36 +0545

    Add allocation statistics tracking module
    - Implement AllocatorStats structure for usage metrics
    - Add StatsTracker class to record allocations and frees
    - Track current and peak memory usage
    - Implement printStats() for reporting allocator statistics

---
+++ Custom_Memory_Allocator_Project/include/stats_tracking.hpp
@@ -0,0 +1,30 @@
+#ifndef STATS_TRACKING_HPP
+#define STATS_TRACKING_HPP
+
+#include <cstddef>
+
+struct AllocatorStats {
+  size_t totalAllocations; // total number of times allocate() is called
+  size_t totalFrees;       // total number of times free() is called
+
+  size_t currentMemoryUsage; // how much memory is in use right now
+  size_t peakMemoryUsage;    // The highest memory usage ever
+
+  size_t totalFreeMemory; // how much free memory is available
+};
+
+class StatsTracker {
+public:
+  StatsTracker(); // constructor- starts tracking from zero
+
+  void recordAllocation(size_t size); // call this when someone allocates
+  void recordFree(size_t size);       // call this when someone frees
+
+  void printStats(); // print the current stats
+
+private:
+  AllocatorStats stats; // creating a variable from the type AllocatorStats we
+                        // made using struct
+};
+
+#endif
---
+++ Custom_Memory_Allocator_Project/src/stats_tracking.cpp
@@ -0,0 +1,38 @@
+#include "../include/stats_tracking.hpp"
+#include <iostream>
+
+// initializing all the stats to zero
+StatsTracker::StatsTracker() {
+  stats = {
+      0,
+      0,
+      0,
+      0,
+  };
+}
+// making constructors
+void StatsTracker::recordAllocation(size_t size) {
+  stats.totalAllocations++;
+  stats.currentMemoryUsage += size;
+  // stats.totalFreeMemory -= size;
+
+  if (stats.currentMemoryUsage > stats.peakMemoryUsage) {
+    stats.peakMemoryUsage = stats.currentMemoryUsage;
+  }
+}
+
+void StatsTracker::recordFree(size_t size) {
+
+  stats.totalFrees++;
+  stats.currentMemoryUsage -= size;
+  //  stats.totalFreeMemory += size; // freeing memory adds free sapce
+}
+
+void StatsTracker::printStats() {
+  std::cout << "Total Allocations: " << stats.totalAllocations << std::endl;
+  std::cout << "Total Frees: " << stats.totalFrees << std::endl;
+  std::cout << "Current memory usage: " << stats.currentMemoryUsage
+            << std::endl;
+  std::cout << "Peak memory usage: " << stats.peakMemoryUsage << std::endl;
+  // std::cout << "Total free memory: " << stats.totalFreeMemory << std::endl;
+}

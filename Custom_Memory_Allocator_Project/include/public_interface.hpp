e1781f6a639813264cdb4649f77ae24875019c43
AryanPaudel8 <rnrn97967@gmail.com>
2026-03-10 19:05:24 +0545

    Add public MemoryAllocator interface
    - Define MemoryAllocator class
    - Declare allocate() and freeMemory() functions
    - Add constructor and destructor for memory pool management
    - Provide printStats() interface for allocator statistics

---
+++ Custom_Memory_Allocator_Project/include/public_interface.hpp
@@ -0,0 +1,30 @@
+#ifndef PUBLIC_INTERFACE_HPP
+#define PUBLIC_INTERFACE_HPP
+
+#include <cstddef> // this header gives us the standard c++ defn for size related typoes and constants in our case for (size_t).
+
+// Defining the functions that my allocator will do or call.
+
+class MemoryAllocator {
+public:
+  // Constructopr for our memory allocator
+  MemoryAllocator(size_t poolSize);
+
+  // Destructor for our memory allocator
+  ~MemoryAllocator();
+
+  // To allocate memory
+  void *allocate(size_t size);
+
+  // For freeing memory
+  void freeMemory(void *ptr);
+
+  // To print the statsics
+  void printStats();
+
+private:
+  void *poolStart; // where our memory pool begins
+  size_t poolSize; // How big our memory pool is
+};
+
+#endif
